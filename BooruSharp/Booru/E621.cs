d7775fc38700da657dbe3f2d60cb0c1e6e1bb8ec
Xwilarg <xwilarg@yahoo.fr>
2020-03-12 17:21:17 +0100

    Fix E621 URL

--- BooruSharp/Booru/E621.cs
+++ BooruSharp/Booru/E621.cs
@@ -2,7 +2,7 @@
 {
     public sealed class E621 : Template.E621
     {
-        public E621(BooruAuth auth = null) : base("beta.e621.net", auth)
+        public E621(BooruAuth auth = null) : base("e621.net", auth)
         { }
 
         public override bool IsSafe()
