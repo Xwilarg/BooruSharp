464dea52c321723c7653bc1d0dc9fff440f8e302
Xwilarg <xwilarg@yahoo.fr>
2020-08-19 17:23:18 +0200

    Fix Rule34 random search

--- BooruSharp/Booru/Rule34.cs
+++ BooruSharp/Booru/Rule34.cs
@@ -2,7 +2,7 @@
 {
     public sealed class Rule34 : Template.Gelbooru02
     {
-        public Rule34() : base("rule34.xxx", BooruOptions.noComment)
+        public Rule34() : base("rule34.xxx", BooruOptions.noComment | BooruOptions.limitOf20000) // The limit is in fact 200000 but search with tags make it incredibly hard to know what is really your pid
         { }
 
         public override bool IsSafe()
