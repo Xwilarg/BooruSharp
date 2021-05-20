bcca11408ff31ce9f0a7e316d0f90b585ef82278
Xwilarg <christian.chaux@bionomeex.com>
2021-05-20 17:22:51 +0200

    Add tags for GetLastPostsAsync

--- BooruSharp.Others/Furaffinity.cs
+++ BooruSharp.Others/Furaffinity.cs
@@ -3,6 +3,7 @@ using BooruSharp.Search;
 using BooruSharp.Search.Post;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
@@ -42,13 +43,20 @@ namespace BooruSharp.Others
         /// <exception cref="InvalidTags"/>
         public override async Task<SearchResult[]> GetLastPostsAsync(params string[] tagsArg)
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, "https://www.furaffinity.net/browse/");
+            string endpoint = tagsArg.Length > 0 ? "search" : "browse";
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://www.furaffinity.net/" + endpoint);
+            if (tagsArg.Length > 0)
+            {
+                MultipartFormDataContent formData = new MultipartFormDataContent();
+                formData.Add(new StringContent("@keywords+" + string.Join("+", tagsArg)), "q");
+                request.Content = formData;
+            }
             if (_cookieA != null)
             {
                 request.Content = new FormUrlEncodedContent(
                     new Dictionary<string, string>
                     {
-                    { "Cookie", "a=" + _cookieA + ";b=" + _cookieB }
+                        { "Cookie", "a=" + _cookieA + ";b=" + _cookieB }
                     });
             }
             var req = await HttpClient.SendAsync(request);
@@ -56,7 +64,7 @@ namespace BooruSharp.Others
             List<SearchResult> res = new List<SearchResult>();
 
             var matches = Regex.Matches(html,
-                "<figure id=\"sid-[0-9]+\" class=\"([^\"]+)\"><b><u><a href=\"\\/view\\/([0-9]+)\\/\"><img alt=\"\" src=\"([^\"]+)  data-width=\"([0-9]+)\" data-height=\"([0-9]+)\"");
+                "<figure id=\"sid-[0-9]+\" class=\"([^\"]+)\"><b><u><a href=\"\\/view\\/([0-9]+)\\/\"><img alt=\"\" src=\"([^\"]+)\"  data-width=\"([0-9.]+)\" data-height=\"([0-9.]+)\"");
             foreach (var match in matches.Cast<Match>())
             {
                 var id = match.Groups[2].Value;
@@ -66,13 +74,13 @@ namespace BooruSharp.Others
                         new Uri("https:" + match.Groups[3].Value),
                         new Uri("https://www.furaffinity.net/view/" + id + "/"),
                         match.Groups[1].Value.Contains("r-adult") ? Rating.Explicit : Rating.Safe,
-                        null,
+                        Array.Empty<string>(),
                         int.Parse(id),
                         null,
                         -1,
                         -1,
-                        int.Parse(match.Groups[5].Value),
-                        int.Parse(match.Groups[4].Value),
+                        (int)Math.Ceiling(float.Parse(match.Groups[5].Value, CultureInfo.InvariantCulture)),
+                        (int)Math.Ceiling(float.Parse(match.Groups[4].Value, CultureInfo.InvariantCulture)),
                         null,
                         null,
                         null,
