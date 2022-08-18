330d62e1e1d0ba44a0617979acde5bc26360e4c5
Xwilarg <xwilarg@protonmail.com>
2022-08-18 13:59:22 +0200

    Fix Ponybooru tags not being found

--- BooruSharp/Booru/Ponybooru.cs
+++ BooruSharp/Booru/Ponybooru.cs
@@ -1,4 +1,7 @@
 ﻿using BooruSharp.Booru.Template;
+using System;
+using System.Net.Http;
+using System.Web;
 
 namespace BooruSharp.Booru
 {
@@ -19,5 +22,21 @@ namespace BooruSharp.Booru
 
         /// <inheritdoc/>
         protected override int FilterID => 2;
+
+        protected override void PreRequest(HttpRequestMessage message)
+        {
+            var uriBuilder = new UriBuilder(message.RequestUri.AbsoluteUri);
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            if (Auth != null)
+            {
+                query["key"] = Auth.PasswordHash;
+            }
+            else
+            {
+                query["filter_id"] = $"{FilterID}"; // filter 2 for Ponybooru still hide stuff so we only add it if auth isn't given
+            }
+            uriBuilder.Query = query.ToString();
+            message.RequestUri = new Uri(uriBuilder.ToString());
+        }
     }
 }
