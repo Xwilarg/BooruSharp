6129e13dbb22451ed2fc1079a74f55c3d974d5a2
Xwilarg <xwilarg@protonmail.com>
2023-04-12 19:02:45 +0200

    Fix crash for multiple image search

--- BooruSharp/Booru/Impl/Rule34.cs
+++ BooruSharp/Booru/Impl/Rule34.cs
@@ -28,7 +28,7 @@ namespace BooruSharp.Booru
         {
             // We don't have to handle what happen when there is no tag because it'll throw before
 
-            var url = new Uri($"{_imageUrl}&limit=1&id={string.Join("+", tags.Select(Uri.EscapeDataString)).ToLowerInvariant()}&json=0");
+            var url = new Uri($"{_imageUrl}&limit=1&tags={string.Join("+", tags.Select(Uri.EscapeDataString)).ToLowerInvariant()}&json=0");
             XmlDocument xml = await GetXmlAsync(url.AbsoluteUri);
             int max = int.Parse(xml.ChildNodes.Item(1).Attributes[0].InnerXml);
 
